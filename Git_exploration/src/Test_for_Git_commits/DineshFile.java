6484abf82743a7ed1c312bdb7bd445f2274ba566
sarathranjith <sarathranjith@192.168.0.4>
2019-05-18 12:51:50 +0530

    Removed code from sarath file

--- Git_exploration/src/Test_for_Git_commits/DineshFile.java
+++ Git_exploration/src/Test_for_Git_commits/DineshFile.java
@@ -5,7 +5,11 @@ public class DineshFile {
 	public static void main(String[] args) {
 		System.out.println("This is Dinesh File");
 		
-		System.out.println("Added by sarath");
+		//System.out.println("Added by sarath");
+		
+		//Addedd by sarath
+		int b = 20;
+		
 	}
 
 }
--- Git_exploration/src/Test_for_Git_commits/Sarath_file.java
+++ Git_exploration/src/Test_for_Git_commits/Sarath_file.java
@@ -3,10 +3,8 @@ package Test_for_Git_commits;
 public class Sarath_file {
 
 	public static void main(String[] args) {
-	int a = 10;
-	System.out.println("Sarath_File");
-
-	System.out.println("Added by sarath");
+	
+	
 	}
 
 }
